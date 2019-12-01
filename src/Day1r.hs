0fa1e15b56aa3e90e822425ac72c36e317108133
Bjørn Arild Kristiansen <bjorn.arild.kristiansen@finn.no>
2019-12-01 21:40:44 +0100

    Format & fix

--- src/Day1r.hs
+++ src/Day1r.hs
@@ -1,25 +1,23 @@
 module Day1r where
 
-  import FileParser
+import           FileParser
 
-  calcRequiredFuel1 mass = (mass `div` 3) - 2
-  
-  calcRequiredFuel2 x =
-    let y = calcRequiredFuel1 x in
-    if y <= 0
-    then 0
-    else y + (calcRequiredFuel2 y)
-  
-  solve f xs = sum $ map f xs
-  
-  main1 = do
-    putStrLn "=== DAY 1-1 ==="
-    input <- parseLines read "src/Day1.dat"
-    let result = solve calcRequiredFuel1 input
-    putStrLn $ show result
-  
-  main2 = do
-    putStrLn "=== DAY 1-2 ==="
-    input <- parseLines read "src/Day1.dat"
-    let result = solve calcRequiredFuel2 input
-    putStrLn $ show result
+calcRequiredFuel1 mass = (mass `div` 3) - 2
+
+calcRequiredFuel2 x =
+  let y = calcRequiredFuel1 x
+  in  if y <= 0 then 0 else y + calcRequiredFuel2 y
+
+solve f xs = sum $ map f xs
+
+main1 = do
+  putStrLn "=== DAY 1-1 ==="
+  input <- parseLines read "resources/Day1.dat"
+  let result = solve calcRequiredFuel1 input
+  print result
+
+main2 = do
+  putStrLn "=== DAY 1-2 ==="
+  input <- parseLines read "resources/Day1.dat"
+  let result = solve calcRequiredFuel2 input
+  print result
