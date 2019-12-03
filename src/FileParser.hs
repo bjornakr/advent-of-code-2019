a50f0f837d74d538717a5827cef4b9a0e3a277e4
Bjørn Arild Kristiansen <bjorn.arild.kristiansen@finn.no>
2019-12-03 20:56:26 +0100

    Add D3P1 (solved)

--- src/FileParser.hs
+++ src/FileParser.hs
@@ -1,5 +1,18 @@
 module FileParser where
 
+import Data.List.Split (splitOn)
+
 parseLines f filename = do
   inputRaw <- readFile filename
   pure $ fmap f (lines inputRaw)
+
+parseCsv = splitOn ","
+
+parseCsvLines :: (String -> a) -> String -> IO [[a]]
+parseCsvLines f filename = do
+  lines <- parseLines id filename  
+  
+  let 
+    splits :: [[String]]
+    splits = fmap (splitOn ",") lines
+  pure $ fmap (fmap f) splits
